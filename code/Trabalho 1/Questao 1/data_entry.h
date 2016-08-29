4986dd1979c33b7deba403a9302a98f9298fc40e
Luciene <marialucienefelix@gmail.com>
2016-08-29 08:48:36 -0300

    Questão 1 99% finalizada

---
+++ code/Trabalho 1/Questao 1/data_entry.h
@@ -0,0 +1,2 @@
+void input_data(Triangle *triangle);
+void print_triangle_coordinates(Triangle triangle);
---
+++ code/Trabalho 1/Questao 1/geometric_operators.h
@@ -0,0 +1,6 @@
+void print_triangle_sides(Triangle triangle);
+float calcule_triangle_perimeter(Triangle triangle);
+float triangle_side(Point point_1, Point point_2);
+void set_triangle_sides(Triangle *triangle);
+int validade_triangle(Triangle triangle);
+float calcule_triangle_area(Triangle triangle);
